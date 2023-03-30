efeb5ecc7cc102139c2ca1468476eb4c37785982
Galazka Urszula <jakula@poczta.fm>
2023-03-30 14:53:57 +0200

    Update On file

--- Examples/L5/WriteAPoem.java
+++ Examples/L5/WriteAPoem.java
@@ -1,12 +1,14 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class WriteAPoem {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		/* 1. we want to write verses of poem to file 
 		 * 2. and we want to read it from file 
-		 * 3. and print with division to group of verses */
-		
+		 * 3. and print with division to group of verses */	
 		try {
 			FileWriter file = new FileWriter("poem.txt");
 			file.write("Today is a october day\n");
@@ -18,6 +20,16 @@ public class WriteAPoem {
 		}catch (IOException err) {
 			System.out.println("I cannot write to file\n");
 			err.printStackTrace();
+		}
+		
+		File fileToRead = new File("poem.txt");
+		Scanner read = new Scanner(fileToRead);
+		int i=1;
+		while(read.hasNextLine()) {
+			System.out.println(read.nextLine());
+			i++;
+			if(i%2==1)
+				System.out.println();
 		}
 		
 	}
